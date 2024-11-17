69d6bc0da63f1cae317c76c0f72d744d02d221d1
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-17 10:33:18 +0300

    Sorted configuration for cloudinary

--- src/blogs/utils/imageUpload.ts
+++ src/blogs/utils/imageUpload.ts
@@ -1,15 +1,15 @@
-import cloudinary from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
 // Function to upload an image to Cloudinary
 export const uploadImage = async (file: Express.Multer.File) => {
   try {
-    const result = await cloudinary.v2.uploader.upload(file.path, {
+    const result = await cloudinary.uploader.upload(file.path, {
       folder: 'blog_images',
     });
     return result.secure_url; // Return the URL of the uploaded image
