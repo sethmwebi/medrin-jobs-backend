691de336095e415ea140d96f223c399628fc5b0e
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-17 10:09:14 +0300

    Using cloudinary to allow upload of images to our database. Also managed . Will be creating an account for it

---
+++ src/blogs/utils/imageUpload.ts
@@ -0,0 +1,19 @@
+import cloudinary from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Function to upload an image to Cloudinary
+export const uploadImage = async (file: Express.Multer.File) => {
+  try {
+    const result = await cloudinary.v2.uploader.upload(file.path, {
+      folder: 'blog_images',
+    });
+    return result.secure_url; // Return the URL of the uploaded image
+  } catch (error) {
+    throw new Error('Image upload failed');
+  }
+};
