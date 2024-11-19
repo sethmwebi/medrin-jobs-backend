c74ff40c4737abbfd8e671b9cd76da962cdd75e0
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2024-11-19 16:07:29 +0300

    Adds payment API endpoints
    Adds endpoints for creating payment intents, handling payment success, creating subscriptions, canceling subscriptions, and processing mobile payments using STK Push.

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -4,13 +4,16 @@ import express, { RequestHandler } from "express";
 import {
 	createPaymentIntent,
 	handlePaymentSuccess,
-	createSubscription
+	createSubscription,
+	stkPush,
 } from "../controllers/payment";
 
 const router = express.Router();
 
-router.post("/payment-intent", createPaymentIntent as unknown as RequestHandler);
+router.post("/payment-intent",createPaymentIntent as unknown as RequestHandler);
 router.post("/payment-success", handlePaymentSuccess);
 router.post("/create-subscription", createSubscription);
+router.post("/cancel-subscription", createSubscription);
+router.post("/pay", stkPush as unknown as RequestHandler);
 
 export default router;
