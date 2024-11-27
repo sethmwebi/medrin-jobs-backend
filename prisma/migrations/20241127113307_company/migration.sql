54a8de440e7f1fde01a6f1b9fe1d6355596fbceb
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2024-11-27 15:23:10 +0300

    Adds company details to user model
    Adds columns for company location, name, size, website, industry, and phone number to the users table.  This allows for richer user profiles and more comprehensive data collection.

---
+++ prisma/migrations/20241127113307_company/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "companyLocation" TEXT,
+ADD COLUMN     "companyName" TEXT,
+ADD COLUMN     "companySize" TEXT,
+ADD COLUMN     "companyWebsite" TEXT,
+ADD COLUMN     "industry" TEXT,
+ADD COLUMN     "phoneNumber" INTEGER;
