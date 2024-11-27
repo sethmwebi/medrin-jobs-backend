2c1069b2a62012cb5f07dc0d3228ce01f9bb5099
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2024-11-27 16:09:32 +0300

    Adds employer update routes
    Adds endpoints for employer profile viewing and details updating.

---
+++ src/routes/employerUpdate.ts
@@ -0,0 +1,6 @@
+import express, { RequestHandler } from "express";
+import *  as employerController from "../controllers/companyDetails";
+
+const router =express.Router()
+router.get("/profile",employerController.getUserProfile as unknown as RequestHandler);
+router.put("/update-details",employerController.updateEmployerDetails as unknown as RequestHandler);
